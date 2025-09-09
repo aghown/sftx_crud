032b9ad2f579b77feef0b84061dc234db7646976
Juliandre César <77554865+Burhkardt@users.noreply.github.com>
2025-09-09 19:44:41 +0000

    Adicionado laço de repetição

--- src/menu.py
+++ src/menu.py
@@ -10,33 +10,34 @@ import functions.search as Search
 
 
 def OpenMenu():
-    print("O que você deseja fazer?\n" \
-          "1 - Adicionar empreendedor\n" \
-          "2 - Atualizar empreendedor\n" \
-          "3 - Remover empreendedor\n" \
-          "4 - Pesquisar lista de empreendedores\n" \
-          "5 - Procurar empreendedor\n" \
-          "0 - Cancelar")
-    choice = input()
-    match choice:
-        case "1":
-            lista = Save()
-            nome = lista[0]
-            cidade = lista[1]
-            contato = lista[2]
-            cnpj = lista[3]
-            setor = lista[4]
-            Create.criar_empreendedor(empreendedores, nome, cidade, contato, cnpj, setor)
-        case "2":
-            Update.atualizar_empreendedor(empreendedores)
-        case "3":
-            Delete.RemoveData(empreendedores)
-        case "4":
-            List.ListData(empreendedores)
-        case "5":
-            procurado = input("Digite o nome procurado: ")
-            Search.procurar_empreendedores(empreendedores, procurado)
-        case "0":
-            pass
-        case _:
-            print("Escolha uma opção válida")
+    while True:
+        print("O que você deseja fazer?\n" \
+            "1 - Adicionar empreendedor\n" \
+            "2 - Atualizar empreendedor\n" \
+            "3 - Remover empreendedor\n" \
+            "4 - Pesquisar lista de empreendedores\n" \
+            "5 - Procurar empreendedor\n" \
+            "0 - Cancelar")
+        choice = input()
+        match choice:
+            case "1":
+                lista = Save()
+                nome = lista[0]
+                cidade = lista[1]
+                contato = lista[2]
+                cnpj = lista[3]
+                setor = lista[4]
+                Create.criar_empreendedor(empreendedores, nome, cidade, contato, cnpj, setor)
+            case "2":
+                Update.atualizar_empreendedor(empreendedores)
+            case "3":
+                Delete.RemoveData(empreendedores)
+            case "4":
+                List.ListData(empreendedores)
+            case "5":
+                procurado = input("Digite o nome procurado: ")
+                Search.procurar_empreendedores(empreendedores, procurado)
+            case "0":
+                break
+            case _:
+                print("Escolha uma opção válida")
