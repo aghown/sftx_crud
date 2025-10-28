25eea2514bb6ef471e8dbc12d0e1ecd61d4f0f24
Juliandre César <juliandrecesar@gmail.com>
2025-10-28 20:07:49 -0300

    Alteração para compatibilidade dos diferentes códigos

--- src/model/empreendedor.py
+++ src/model/empreendedor.py
@@ -10,5 +10,19 @@ class Empreendedor:
         self.cnpj = cnpj
         self.ramo = ramo
 
+    def atualizar(self, nome = None, cidade = None, email = None, telefone = None, cnpj = None, ramo = None):
+        if nome:
+            self.nome = nome
+        if cidade:
+            self.cidade = cidade
+        if email:
+            self.email = email
+        if telefone:
+            self.telefone = telefone 
+        if cnpj:
+            self.cnpj = cnpj
+        if ramo:
+            self.ramo = ramo
+
     def __str__(self):
         return f"Empreendedor({self.id}, {self.nome}, {self.cidade}, {self.email}, {self.telefone}, {self.cnpj}, {self.ramo})"
